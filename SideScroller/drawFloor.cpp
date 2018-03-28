c6b3053f8691b311118e60757737c2b83584d8e6
Unknown <Kyle.e.newton@wsu.edu>
2018-03-28 13:23:20 -0700

    Floor Generation Code

--- SideScroller/drawFloor.cpp
+++ SideScroller/drawFloor.cpp
@@ -1,9 +1,14 @@
 #include "drawFloor.h"
 
 void drawFloor(sf::RenderWindow &window) {
-	sf::RectangleShape Rect = sf::RectangleShape();
-	Rect.setFillColor(sf::Color::Black);
-	Rect.setSize(sf::Vector2f(800, 50));
-	Rect.setPosition(sf::Vector2f(0, 550));
-	window.draw(Rect);
+	sf::Texture brick;
+	if (!brick.loadFromFile("Brick1.png")) {}
+
+	for (int i = 0; i < 50; i++) {
+		sf::RectangleShape Rect = sf::RectangleShape();
+		Rect.setTexture(&brick);
+		Rect.setSize(sf::Vector2f(32, 32));
+		Rect.setPosition(sf::Vector2f(0 + (i*16), 568));
+		window.draw(Rect);
+	}
 }
