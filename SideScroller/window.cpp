d82cf043dd8ca3bbdf6f7f9356e8cd43c9b6b9f6
Unknown <Kyle.e.newton@wsu.edu>
2018-03-29 20:10:29 -0700

    Created A Window Class to draw each window

--- SideScroller/window.cpp
+++ SideScroller/window.cpp
@@ -25,4 +25,22 @@ void drawWindow(sf::RenderWindow &window) {
 }
 void drawMenu(sf::RenderWindow &window) {
 
+	sf::Texture background;
+	Character p1;
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+
+			window.setTitle("2D Side Scroller");
+			window.setVerticalSyncEnabled(true);
+			if (!background.loadFromFile("resources/BackGround.png")) {}
+			sf::Sprite background(background);
+			window.draw(background);
+			window.display();
+		}
+	}
 }
